3f18a03cf93985895913e83cd38df35bde9aa492
Shrey Gupta <shreygupta97@gmail.com>
2019-01-07 22:17:31 -0800

    Added support for D3.js Pie Charts.

--- js/app.js
+++ js/app.js
@@ -23,20 +23,20 @@ $(document).ready(function(){
   $('form#signup-form').submit(function(e){
     e.preventDefault();
     $('.mailing-list-feedback').remove();
-    var jqxhr = $.ajax({
+    let jqxhr = $.ajax({
       url: "https://script.google.com/macros/s/AKfycbx4GUdH58BkYER1OmIi3zGBEpYghnEkVdkL1VxKIvrMzUQ725o/exec",
       method: "GET",
       dataType: "json",
       data: $("form#signup-form").serialize()
     }).done(function() {
-      var res = document.createElement("p");
-      var textnode = document.createTextNode("Thanks!");
+      let res = document.createElement("p");
+      let textnode = document.createTextNode("Thanks!");
       res.appendChild(textnode);
       res.classList.add("mailing-list-feedback");
       $("#mailing-list").append(res);
     }).fail(function() {
-      var res = document.createElement("p");
-      var textnode = document.createTextNode("An error occurred. Please try again later.");
+      let res = document.createElement("p");
+      let textnode = document.createTextNode("An error occurred. Please try again later.");
       res.appendChild(textnode);
       res.classList.add("mailing-list-feedback");
       $("#mailing-list").append(res);
@@ -50,12 +50,18 @@ $(document).ready(function(){
   $('#signup-button').mouseleave(function(){
     $(this).animate({'opacity': '1'}, 300);
   });
+
+  data = [{"label": "Leader", "value": 50, "color": "#0d1ccd"},
+          {"label": "Mediator", "value": 28, "color": "#1426f9"},
+          {"label": "Listener", "value": 22, "color": "#3d43fb"}].reverse();
+
+  makeSVGPieChart("#pieChart", data);
 });
 
 // Controls arrow hopping animation when mouse hovers over
-var lastCall = 0;
+let lastCall = 0;
 function arrowHop() {
-  var now = Date.now();
+  let now = Date.now();
   if (lastCall + 1000 < now) {
     lastCall = now;
     $('.arrow').finish().show()
@@ -63,4 +69,47 @@ function arrowHop() {
       $(this).animate({'margin-bottom': '0px'}, 350);
     });
   }
+}
+
+function makeSVGPieChart(element, data) {
+  RADIUS = 100;
+  MARGIN = 5;
+  MARGIN_MULT = 1.2;
+
+  let vis = d3.select(element)
+    .append("svg:svg")
+      .data([data]).attr("width", 2 * RADIUS).attr("height", 2 * RADIUS)
+    .append("svg:g")
+      .attr("transform", `translate(${RADIUS},${RADIUS})`);
+
+  let arc = d3.arc().outerRadius(RADIUS).innerRadius(0);
+
+  let pie = d3.pie().sort(null).value(function(d) { return d.value; });
+
+  let arcs = vis.selectAll("g.slice").data(pie).enter()
+    .append("svg:g")
+      .attr("class", "slice");
+
+  arcs.append("svg:path")
+    .attr("fill", d => d.data.color)
+    .attr("d", arc);
+
+  let texts = arcs.append("svg:text")
+  texts.attr("text-anchor", "middle").attr("dy", "0").attr("font-size", "18");
+
+  texts.append('tspan')
+    .text(d => d.data.label)
+    .attr("dy", "0em").attr("x", "0");
+  texts.append('tspan')
+    .text(d => d.data.value + "%")
+    .attr("dy", "1.1em").attr("x", "0");
+
+  texts.attr("transform", function(d) {
+    let [x, y] = arc.centroid(d);
+    if ((x > 0 && x - this.getBBox().width / 2 < MARGIN) ||
+        (x < 0 && x + this.getBBox().width / 2 > -MARGIN)){
+      x *= MARGIN_MULT
+    }
+    return `translate(${x},${y})`;
+  });
 }
