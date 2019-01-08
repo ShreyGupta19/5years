2ede46ddeee8f13ec135b954078b52eb787bdc03
Shrey Gupta <shreygupta97@gmail.com>
2019-01-07 23:04:10 -0800

    Added styling and transitions for pie chart.

--- js/app.js
+++ js/app.js
@@ -88,10 +88,17 @@ function makeSVGPieChart(element, data) {
 
   let arcs = vis.selectAll("g.slice").data(pie).enter()
     .append("svg:g")
-      .attr("class", "slice");
+      .attr("class", "slice")
+    .on("mouseover", function(d) {
+      d3.select(this).select("path").transition().style("fill", d3.rgb(d.data.color).brighter(2).hex())
+        .duration(400).ease(d3.easeCubicOut)
+    }).on("mouseout", function(d) {
+      d3.select(this).select("path").transition().style("fill", d.data.color)
+        .duration(400).ease(d3.easeCubicOut)
+    });
 
   arcs.append("svg:path")
-    .attr("fill", d => d.data.color)
+    .style("fill", d => d.data.color)
     .attr("d", arc);
 
   let texts = arcs.append("svg:text")
@@ -99,10 +106,11 @@ function makeSVGPieChart(element, data) {
 
   texts.append('tspan')
     .text(d => d.data.label)
-    .attr("dy", "0em").attr("x", "0");
+    .attr("dy", "0em").attr("x", "0").attr("class", "pie-label");
   texts.append('tspan')
     .text(d => d.data.value + "%")
-    .attr("dy", "1.1em").attr("x", "0");
+    .attr("dy", "1.1em").attr("x", "0").attr("class", "pie-percent")
+    .attr("font-size", "0.8em");
 
   texts.attr("transform", function(d) {
     let [x, y] = arc.centroid(d);
