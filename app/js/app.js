a1564473cfa59f9a6c782daf702c37d1601f222e
Shrey Gupta <shreygupta97@gmail.com>
2019-01-09 20:30:41 -0800

    Left-aligned pie charts.

--- app/js/app.js
+++ app/js/app.js
@@ -62,67 +62,74 @@ $(document).ready(function(){
   makeSVGPieChart("#identity-male-pie", [
     {"label": "Leader", "value": 50, "color": "#0702d1", "lcolor": "#f7f7f8"},
     {"label": "Listener", "value": 22, "color": "#3b38ff", "lcolor": "#f7f7f8",
-      "xMargin": 1.25, "yMargin": 1.1},
+      "fontSize": 0.9, "xMargin": 1.3, "yMargin": 1.1},
     {"label": "Mediator", "value": 28, "color": "#0c08fd", "lcolor": "#f7f7f8",
-      "xMargin": 1.1},
+      "fontSize": 0.85, "xMargin": 1.1, "yMargin": 0.9},
   ].reverse());
 
   makeSVGPieChart("#identity-female-pie", [
     {"label": "Leader", "value": 36, "color": "#0702d1", "lcolor": "#f7f7f8"},
     {"label": "Listener", "value": 46, "color": "#3b38ff", "lcolor": "#f7f7f8"},
     {"label": "Mediator", "value": 18, "color": "#0c08fd", "lcolor": "#f7f7f8",
-      "fontSize": 0.9, "xMargin": 1.4, "yMargin": 1.1},
+      "fontSize": 0.75, "xMargin": 1.4, "yMargin": 1.1},
   ].reverse());
 
   makeSVGPieChart("#future-male-pie", [
     {"label": "Founder", "value": 50, "color": "#0702d1", "lcolor": "#f7f7f8"},
     {"label": "VC", "value": 35, "color": "#3b38ff", "lcolor": "#f7f7f8"},
     {"label": "Other", "value": 15, "color": "#0c08fd", "lcolor": "#f7f7f8",
-      "xMargin": 1.25, "yMargin": 1.2},
+      "fontSize": 0.95, "xMargin": 1.4, "yMargin": 1.2},
   ].reverse());
 
   makeSVGPieChart("#future-female-pie", [
     {"label": "Founder", "value": 27, "color": "#0702d1", "lcolor": "#f7f7f8",
-      "xMargin": 1.1},
+      "fontSize": 0.9, "xMargin": 1.2, "yMargin": 0.9},
     {"label": "VC", "value": 45, "color": "#3b38ff", "lcolor": "#f7f7f8"},
-    {"label": "Other", "value": 28, "color": "#0c08fd", "lcolor": "#f7f7f8"},
+    {"label": "Other", "value": 28, "color": "#0c08fd", "lcolor": "#f7f7f8",
+      "xMargin": 1.1, "yMargin": 1.1},
   ].reverse());
 
   makeSVGPieChart("#capable-male-pie", [
     {"label": "As Capable", "value": 65, "color": "#1dd79d",
-      "lcolor": "#000000"},
+      "lcolor": "#000000", "xMargin": 0.6, "yMargin": 0.9},
     {"label": "Somewhat More Capable", "value": 35, "color": "#17ab7c",
-      "lcolor": "#000000", "fontSize": 0.9},
+      "lcolor": "#000000", "fontSize": 0.7, "yMargin": 1.1, "lineSpacing": 1.3},
   ].reverse());
 
   makeSVGPieChart("#capable-female-pie", [
     {"label": "As Capable", "value": 83, "color": "#1dd79d",
-      "lcolor": "#000000"},
+      "lcolor": "#000000", "xMargin": 0.6, "yMargin": -1.5},
     {"label": "Somewhat More Capable", "value": 17, "color": "#17ab7c",
-      "lcolor": "#000000", "fontSize": 0.75, "xMargin": 1.35, "yMargin": 1.1},
+      "lcolor": "#000000", "fontSize": 0.6, "xMargin": 1.4, "yMargin": 1.1,
+      "lineSpacing": 1.6},
   ].reverse());
 
   makeSVGPieChart("#sexhar-scope-drf", [
-    {"label": "Accurate", "value": 33, "color": "#da1301", "lcolor": "#f7f7f8"},
+    {"label": "Accurate", "value": 33, "color": "#da1301", "lcolor": "#f7f7f8",
+      "fontSize": 0.9, "xMargin": 1.1},
     {"label": "Less", "value": 9, "color": "#fe3c2a", "lcolor": "#f7f7f8",
-      "xMargin": 1.4, "yMargin": 1.8, "fontSize": 0.9},
+      "xMargin": 1.3, "yMargin": 1.9, "fontSize": 0.85},
     {"label": "More", "value": 58, "color": "#8c0c01", "lcolor": "#f7f7f8"},
   ].reverse())
 
   makeSVGPieChart("#sexhar-work-drf", [
-    {"label": "No", "value": 27, "color": "#da1301", "lcolor": "#f7f7f8"},
+    {"label": "No", "value": 27, "color": "#da1301", "lcolor": "#f7f7f8",
+      "xMargin": 1.1, "yMargin": 1.1},
     {"label": "Yes", "value": 73, "color": "#8c0c01", "lcolor": "#f7f7f8"},
   ].reverse())
 
   makeSVGPieChart("#sexhar-scope-founders", [
-    {"label": "Accurate", "value": 37, "color": "#da1301", "lcolor": "#f7f7f8"},
+    {"label": "Accurate", "value": 37, "color": "#da1301", "lcolor": "#f7f7f8",
+      "fontSize": 0.9, "xMargin": 1.05},
     {"label": "Less", "value": 34, "color": "#fe3c2a", "lcolor": "#f7f7f8"},
-    {"label": "More", "value": 29, "color": "#8c0c01", "lcolor": "#f7f7f8"},
+    {"label": "More", "value": 29, "color": "#8c0c01", "lcolor": "#f7f7f8",
+      "xMargin": 1.1, "yMargin": 1.1},
   ].reverse())
 
   makeSVGPieChart("#sexhar-work-founders", [
     {"label": "No", "value": 40, "color": "#da1301", "lcolor": "#f7f7f8"},
-    {"label": "Yes", "value": 60, "color": "#8c0c01", "lcolor": "#f7f7f8"},
+    {"label": "Yes", "value": 60, "color": "#8c0c01", "lcolor": "#f7f7f8",
+      "yMargin": 2.5},
   ].reverse())
 });
 
@@ -219,33 +226,36 @@ function makeSVGPieChart(element, data) {
     .attr("d", arc);
 
   let texts = arcs.append("svg:text")
-  texts.attr("text-anchor", "middle").attr("dy", "0").attr("font-size", "17");
+  texts.attr("text-anchor", "start").attr("dy", "0").attr("font-size", "22");
 
   texts.each(function(d) {
     let fontSize = d.data.fontSize || 1.0
+    let lineSpacing = d.data.lineSpacing || 1.1
+    let percentSpacing = lineSpacing + 0.2
     for (var [idx, word] of d.data.label.split(" ").entries()) {
       d3.select(this).append('tspan')
         .text(word).style("fill", d.data.lcolor)
         .attr("x", "0").attr("class", "pie-label")
-        .attr("dy", fontSize * 1.1 + "em")
+        .attr("dy", fontSize * lineSpacing + "em")
         .attr("font-size", fontSize + "em");
     }
     d3.select(this).append('tspan')
       .text(d.data.value + "%").style("fill", d.data.lcolor)
       .attr("x", "0").attr("class", "pie-percent")
-      .attr("dy", fontSize * 1.3 + "em")
+      .attr("dy", fontSize * percentSpacing + "em")
       .attr("font-size", fontSize * 0.8 + "em");
   });
 
   texts.attr("transform", function(d) {
     let [x, y] = arc.centroid(d);
-    y -= this.getBBox().height / 2
+    y -= this.getBBox().height / 2;
     if (d.data.xMargin){
       x *= d.data.xMargin
     }
     if (d.data.yMargin){
       y *= d.data.yMargin
     }
+    x -= this.getBBox().width / 2;
     return `translate(${x},${y})`;
   });
 }
